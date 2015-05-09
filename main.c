16a57d71ea135ba8e3d0e9aa70eafc8843470bbb
Jonas Schwartze <jschwartze@me.com>
2015-05-10 01:05:38 +0200

    added implementation of strlcpy
    copied from here: https://github.com/ggreer/the_silver_searcher/blob/a43dc8751bdaf922ced6cb207e9738cb58302f08/src/util.c
    
    Signed-off-by: Jonas Schwartze <jschwartze@me.com>

--- main.c
+++ main.c
@@ -72,6 +72,36 @@ struct input_device {
 	char uid[__DEVICE_UID_LENGTH__];
 } device;
 
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dst, const char *src, size_t size) {
+    char *d = dst;
+    const char *s = src;
+    size_t n = size;
+
+    /* Copy as many bytes as will fit */
+    if (n != 0) {
+        while (--n != 0) {
+            if ((*d++ = *s++) == '\0') {
+                break;
+            }
+        }
+    }
+
+    /* Not enough room in dst, add NUL and traverse rest of src */
+    if (n == 0) {
+        if (size != 0) {
+            *d = '\0'; /* NUL-terminate dst */
+        }
+
+        while (*s++) {
+        }
+    }
+
+    return (s - src - 1); /* count does not include NUL */
+}
+#endif
+
+
 char charOf(int key) {
 	switch (key) {
 		case 2:   return '1';
